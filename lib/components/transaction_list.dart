a4e7fad38754256959f8503176a42024aabde83d
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2024-09-20 12:33:15 -0300

    feat: removing SizedBox

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -10,59 +10,56 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 400,
-      child: transactions.isEmpty ? Column(
-        children: <Widget>[
-          const SizedBox(height: 30,),
-          Text(
-            'Nenhuma Transação cadastrada',
-            style: Theme.of(context).textTheme.titleLarge,
+    return transactions.isEmpty ? Column(
+      children: <Widget>[
+        const SizedBox(height: 30,),
+        Text(
+          'Nenhuma Transação cadastrada',
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+        const SizedBox(height: 30,),
+        SizedBox(
+          height: 200,
+          child: Image.asset(
+            'assets\\images\\zzz.png',
+            fit: BoxFit.cover
           ),
-          const SizedBox(height: 30,),
-          SizedBox(
-            height: 200,
-            child: Image.asset(
-              'assets\\images\\zzz.png',
-              fit: BoxFit.cover
-            ),
-          ),
-        ],
-      ) : ListView.builder(
-        itemCount: transactions.length, // itens limitados por tela
-        itemBuilder: (ctx, index) {
-          final e = transactions[index];
-          return Card(
-            elevation: 5,
-            margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-            child: ListTile(
-              leading: CircleAvatar(
-                radius: 30,
-                child: Padding(
-                  padding: const EdgeInsets.all(6.0),
-                  child: FittedBox(
-                    child: Text('R\$ ${e.value}')
-                  ),
+        ),
+      ],
+    ) : ListView.builder(
+      itemCount: transactions.length, // itens limitados por tela
+      itemBuilder: (ctx, index) {
+        final e = transactions[index];
+        return Card(
+          elevation: 5,
+          margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+          child: ListTile(
+            leading: CircleAvatar(
+              radius: 30,
+              child: Padding(
+                padding: const EdgeInsets.all(6.0),
+                child: FittedBox(
+                  child: Text('R\$ ${e.value}')
                 ),
               ),
-              title: Text(
-                e.title,
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
-              subtitle: Text(
-                DateFormat('d/MM/y').format(e.date),
-                style: const TextStyle(
-                  color: Colors.grey,
-                ),
-              ),
-              trailing: IconButton(
-                onPressed: () => onRemove(e.id), 
-                color: Theme.of(context).colorScheme.error,
-                icon: const Icon(Icons.delete)),
             ),
-          );
-        },
-      ),
+            title: Text(
+              e.title,
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            subtitle: Text(
+              DateFormat('d/MM/y').format(e.date),
+              style: const TextStyle(
+                color: Colors.grey,
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () => onRemove(e.id), 
+              color: Theme.of(context).colorScheme.error,
+              icon: const Icon(Icons.delete)),
+          ),
+        );
+      },
     );
   }
 }
