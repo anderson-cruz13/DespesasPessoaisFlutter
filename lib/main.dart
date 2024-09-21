cd10d46b692f5c42900c833dfb49caf2e9bcc1e5
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2024-09-21 14:29:24 -0300

    feat: extraindo MediaQuery.of(context) para uma var final

--- lib/main.dart
+++ lib/main.dart
@@ -97,8 +97,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-
-    bool isLandscape = MediaQuery.of(context).orientation == Orientation.landscape;
+    final mediaQuery = MediaQuery.of(context);
+    bool isLandscape = mediaQuery.orientation == Orientation.landscape;
 
     final appBar = AppBar(
         title: Text(
@@ -125,8 +125,8 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       );
 
-    final availableHeight = MediaQuery.of(context).size.height
-      - appBar.preferredSize.height - MediaQuery.of(context).padding.top;
+    final availableHeight = mediaQuery.size.height
+      - appBar.preferredSize.height - mediaQuery.padding.top;
 
     return Scaffold(
       appBar: appBar,
