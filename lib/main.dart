4b492d86136dd0537b92c52d833d54ae9b50dd8f
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2024-09-21 13:58:39 -0300

    feat: move Switch para appbar

--- lib/main.dart
+++ lib/main.dart
@@ -109,10 +109,19 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         backgroundColor: Theme.of(context).primaryColor,
         actions: <Widget>[
+          if(isLandscape)
+            IconButton(
+              icon: Icon(_showChart ? Icons.list : Icons.show_chart),
+              onPressed: () {
+                setState(() {
+                  _showChart = !_showChart;
+                });
+              },
+            ),
           IconButton(
             icon: const Icon(Icons.add),
             onPressed: () => _openTransactionFormModal(context),
-          )
+          ),
         ],
       );
 
@@ -125,20 +134,6 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[ 
-            if (isLandscape)
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text("Exibir Gráfico"),
-                  Switch(
-                    value: _showChart, 
-                    onChanged: (value) {
-                      setState(() {
-                        _showChart = value;
-                      });
-                  },),
-                ],
-              ),
             if(_showChart || !isLandscape)
               SizedBox(
               height: availableHeight * (isLandscape? 0.7 : 0.25),
