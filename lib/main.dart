1bc4d292805f83bf91d13cba267b4ca9bca6cc79
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2024-09-21 11:35:54 -0300

    feat: add nova configuração de tamanho dos filhos de body

--- lib/main.dart
+++ lib/main.dart
@@ -123,10 +123,10 @@ class _MyHomePageState extends State<MyHomePage> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[ 
             SizedBox(
-              height: availableHeight * 0.3,
+              height: availableHeight * 0.25,
               child: Chart(recentTransaction: _recentTransactions,)),
             SizedBox(
-              height: availableHeight * 0.7,
+              height: availableHeight * 0.75,
               child: TransactionList(transactions: _transactions, onRemove: _removeTransaction,)),
           ],
         ),
